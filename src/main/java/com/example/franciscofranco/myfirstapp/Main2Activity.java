abf275641a663858140a6e4f333f09e9d4ce31e5
Franco <frfranco@uci.edu>
2016-07-27 08:11:44 -0700

    first commit

---
+++ src/main/java/com/example/franciscofranco/myfirstapp/Main2Activity.java
@@ -0,0 +1,26 @@
+package com.example.franciscofranco.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_main2);
+        layout.addView(textView);
+
+    }
+}
